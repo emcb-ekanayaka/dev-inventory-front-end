0482662f924e07c4f23f1cfa3e242a070f144d7f
emcb-ekanayaka <ekanayakabdr@gmail.com>
2024-12-25 19:47:35 +0530

    Design complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { WarehouseComponent } from './components/warehouse/warehouse.component';
 import { ComwarehouseComponent } from './components/comwarehouse/comwarehouse.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     CompanyComponent,
     WarehouseComponent,
-    ComwarehouseComponent
+    ComwarehouseComponent,
+    DashboardComponent
   ],
   exports:[CompanyComponent,ComwarehouseComponent,WarehouseComponent],
   imports: [
